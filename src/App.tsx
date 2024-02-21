45494e82535d253b515cf803d674cb6ab13cc5a4
Luccas Mateus <luccasmmg@gmail.com>
2024-02-21 19:43:38 -0300

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { DecodedLayer } from "@vizzuality/layer-manager-layers-deckgl";
 import { Layer, LayerManager } from "@vizzuality/layer-manager-react";
 import PluginMapboxGl from "@vizzuality/layer-manager-plugin-mapboxgl";
 import CartoProvider from "@vizzuality/layer-manager-provider-carto";
+import {Fp64Extension} from '@deck.gl/extensions';
 
 const cartoProvider = new CartoProvider();
 
@@ -49,66 +50,61 @@ const gladFunction = `
   `;
 
 const integratedFunction = `
-// First 6 bits Alpha channel used to individual alert confidence
-    // First two bits (leftmost) are GLAD-L
-    // Next, 3rd and 4th bits are GLAD-S2
-    // Finally, 5th and 6th bits are RADD
-    // Bits are either: 00 (0, no alerts), 01 (1, low conf), or 10 (2, high conf)
-    // e.g. 00 10 01 00 --> no GLAD-L, high conf GLAD-S2, low conf RADD
+  // First 6 bits Alpha channel used to individual alert confidence
+  // First two bits (leftmost) are GLAD-L
+  // Next, 3rd and 4th bits are GLAD-S2
+  // Finally, 5th and 6th bits are RADD
+  // Bits are either: 00 (0, no alerts), 01 (1, low conf), or 10 (2, high conf)
+  // e.g. 00 10 01 00 --> no GLAD-L, high conf GLAD-S2, low conf RADD
 
-    float agreementValue = alpha * 255.;
+  float agreementValue = alpha * 255.;
 
-    float r = color.r * 255.;
-    float g = color.g * 255.;
-    float b = color.b * 255.;
+  float r = color.r * 255.;
+  float g = color.g * 255.;
+  float b = color.b * 255.;
 
-    float day = r * 255. + g;
-    float confidence = floor(b / 100.) - 1.;
-    // float confidence = 255.;
-    float intensity = mod(b, 100.) * 150.;
-    // float intensity = 255.; //this is temporal above one does not work
+  float day = r * 255. + g;
+  float confidence = floor(b / 100.) - 1.;
+  float intensity = mod(b, 100.) * 50.;
 
-    if (
-      day > 0. &&
-      day >= startDayIndex &&
-      day <= endDayIndex &&
-      agreementValue > 0.
-    )
-    {
-      if (intensity > 255.) {
-        intensity = 255.;
-      }
-      // get high and highest confidence alerts
-      float confidenceValue = 0.;
-      if (confirmedOnly > 0.) {
-        confidenceValue = 255.;
-      }
-
-      if (agreementValue == 4. || agreementValue == 16. || agreementValue == 64.) {
-        // ONE ALERT LOW CONF: 4,8,16,32,64,128 i.e. 2**(2+n) for n<8
-
-        color.r = 237. / 255.;
-        color.g = 164. / 255.;
-        color.b = 194. / 255.;
-        alpha = (intensity -confidenceValue) / 255.;
-      } else if (agreementValue == 8. || agreementValue == 32. || agreementValue ==  128.){
-        // ONE HIGH CONF ALERT: 8,32,128 i.e. 2**(2+n) for n<8 and odd
-
-        color.r = 220. / 255.;
-        color.g = 102. / 255.;
-        color.b = 153. / 255.;
-        alpha = intensity / 255.;
-      } else {
-        // MULTIPLE ALERTS: >0 and not 2**(2+n)
-
-        color.r = 201. / 255.;
-        color.g = 42. / 255.;
-        color.b = 109. / 255.;
-        alpha = intensity / 255.;
-      }
-    } else {
-      alpha = 0.;
+  if (
+    day > 0. &&
+    day >= startDayIndex &&
+    day <= endDayIndex &&
+    agreementValue > 0.
+  )
+  {
+    if (intensity > 255.) {
+      intensity = 255.;
     }
+    // get high and highest confidence alerts
+    float confidenceValue = 0.;
+
+    if (agreementValue == 4. || agreementValue == 16. || agreementValue == 64.) {
+      // ONE ALERT LOW CONF: 4,8,16,32,64,128 i.e. 2**(2+n) for n<8
+
+      color.r = 237. / 255.;
+      color.g = 164. / 255.;
+      color.b = 194. / 255.;
+      alpha = (intensity -confidenceValue) / 255.;
+    } else if (agreementValue == 8. || agreementValue == 32. || agreementValue ==  128.){
+      // ONE HIGH CONF ALERT: 8,32,128 i.e. 2**(2+n) for n<8 and odd
+
+      color.r = 220. / 255.;
+      color.g = 102. / 255.;
+      color.b = 153. / 255.;
+      alpha = intensity / 255.;
+    } else {
+      // MULTIPLE ALERTS: >0 and not 2**(2+n)
+
+      color.r = 201. / 255.;
+      color.g = 42. / 255.;
+      color.b = 109. / 255.;
+      alpha = intensity / 255.;
+    }
+  } else {
+    alpha = 0.;
+  }
  `;
 
 const deckLayer = ({
@@ -129,12 +125,14 @@ const deckLayer = ({
         numberOfDays: 3334,
         confirmedOnly: 0,
       },
+      id: "integrated",
       type: TileLayer,
       data: dataUrl,
       tileSize: 256,
-      refinementStrategy: "no-overlap",
       visible: true,
-      id: "test",
+      refinementStrategy: "no-overlap",
+      extensions: [new Fp64Extension()],
+      opacity: 1,
       renderSubLayers: (sl: any) => {
         const {
           id: subLayerId,
@@ -176,7 +174,7 @@ const deckLayer = ({
         return null;
       },
       minZoom: 3,
-      maxZoom: 20,
+      maxZoom: 12,
     } as any),
   ],
 });
@@ -210,40 +208,40 @@ function App() {
     <>
       <span>{message}</span>
       <div>
-      <button
-        onClick={() => {
-          setMessage("Showing integrated");
-          setAlert(deckLayer(integrated));
-        }}
-      >
-        Show integrated
-      </button>
-      <button
-        onClick={() => {
-          setMessage("Showing glads");
-          setAlert(deckLayer(glads));
-        }}
-      >
-        Show glads
-      </button>
-      <button onClick={() => console.log(logs)}>Log Map</button>
-      <Map
-        {...viewState}
-        ref={(_map) => {
-          if (_map) mapRef.current = _map.getMap() as unknown as MapRef;
-        }}
-        onMove={(evt) => setViewState(evt.viewState)}
-        style={{
-          height: "800px",
-        }}
-        mapStyle="mapbox://styles/mapbox/dark-v11"
-        mapboxAccessToken="pk.eyJ1IjoicmVzb3VyY2V3YXRjaCIsImEiOiJjajFlcXZhNzcwMDBqMzNzMTQ0bDN6Y3U4In0.FRcIP_yusVaAy0mwAX1B8w"
-        onLoad={() => {
-          setReady(true);
-        }}
-      >
-        {ready && <LayerManagerWrapper alert={alert} />}
-      </Map>
+        <button
+          onClick={() => {
+            setMessage("Showing integrated");
+            setAlert(deckLayer(integrated));
+          }}
+        >
+          Show integrated
+        </button>
+        <button
+          onClick={() => {
+            setMessage("Showing glads");
+            setAlert(deckLayer(glads));
+          }}
+        >
+          Show glads
+        </button>
+        <button onClick={() => console.log(logs)}>Log Map</button>
+        <Map
+          {...viewState}
+          ref={(_map) => {
+            if (_map) mapRef.current = _map.getMap() as unknown as MapRef;
+          }}
+          onMove={(evt) => setViewState(evt.viewState)}
+          style={{
+            height: "800px",
+          }}
+          mapStyle="mapbox://styles/mapbox/dark-v11"
+          mapboxAccessToken="pk.eyJ1IjoicmVzb3VyY2V3YXRjaCIsImEiOiJjajFlcXZhNzcwMDBqMzNzMTQ0bDN6Y3U4In0.FRcIP_yusVaAy0mwAX1B8w"
+          onLoad={() => {
+            setReady(true);
+          }}
+        >
+          {ready && <LayerManagerWrapper alert={alert} />}
+        </Map>
       </div>
     </>
   );
